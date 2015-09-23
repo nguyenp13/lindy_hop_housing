fa9a8a2e1e408123cb3af4e4d5aafffcf06da0ce
pn2yr <pn2yr@virginia.edu>
2015-09-23 09:51:56 -0400

    Sstarted working on graphs

--- housing_assignment_generation.py
+++ housing_assignment_generation.py
@@ -3,6 +3,11 @@
 """
 TODO:
     Implement NetworkX code
+    Select an output format
+    Do we want to put extra weight if the guest needs no rides and the host cna't give rides so that we can save the hosts who do gives rides for those guests who need rides? same for all the other questions
+    add late night tendencies to compatibility
+    Would it be a good idea to just assign everyone first and then rearrange them so that more subjective things like coguest preferences and late night tendencies match up?
+    matching means a set of edges with no common vertices. this means that one host can only be connected to one guest. we can alleviate this problem by making copies of  host if he can host more than 1 person. 
 
 Automated Housing Assigment Generator for Lindy Hop Events. 
 
@@ -52,39 +57,97 @@ import pdb
 import networkx
 from util import *
 
-temporary_data_directory = os.path.abspath(generate_unique_directory_name())
+_UNIVERSAL_COUNTER_ = 0
 
-'''
-def housing_csv_to_json(guest_data_csv_file_name, host_data_csv_file_name, output_json_file_name):
-    guest_csv_lines = open(guest_data_csv_file_name, 'r').readlines()
-    keys = [f.strip() for f in csv_lines[0].split(',')]
-    raw_text_data = csv_lines[1:]
-    guests_list=[]
+def get_unique_id_number():
+    global _UNIVERSAL_COUNTER_ 
+    ans = _UNIVERSAL_COUNTER_
+    _UNIVERSAL_COUNTER_ += 1
+    return ans
+
+#temporary_data_directory = os.path.abspath(generate_unique_directory_name())
+
+def host_guest_pretty_print(host_guest):
+    """Pretty prints a host or a guest"""
+    max_key_length = len(max(host_guest.keys(), key=len))
+    print '\n'
+    for key,val in host_guest.items():
+        print ('%'+str(max_key_length)+'s: %s') % (key, val)
+    print '\n'
+
+def parse_tsv_file(tsv_file_name):
+    tsv_lines = open(tsv_file_name, 'r').readlines()
+    keys = [f.strip().lower() for f in tsv_lines[0].split('\t')]
+    raw_text_data = tsv_lines[1:]
+    obj_list=[]
     for line in raw_text_data:
-        guest_data = [f.strip() for f in line.split(',')]
-        guest_object = dict()
-        for key, val in zip(keys,guest_data):
-            guest_object[key] = val
-            guests_list.append(guest_object)
-    pdb.set_trace()
-'''
+        data = [f.strip().lower() for f in line.split('\t')]
+        obj = dict()
+        for key, val in zip(keys,data):
+            obj[key] = val
+        obj['id'] = get_unique_id_number()
+        obj_list.append(obj)
+    return keys, obj_list
+
+def determine_compatibility(host, guest):
+    ans = 0.0
+    # can the host provide rides if the guest needs them?
+    if guest['do you need your host to drive you around?'] == 'yes':
+        if host['are you willing to provide rides to your guests all weekend?'] == 'no':
+            return 0.0
+    
+    # dog allergies 
+    if guest['allergic to dogs?'] == 'yes':
+        if host['do you own dogs?'] == 'yes':
+            return 0.0
+    
+    # cat allergies 
+    if guest['allergic to cats?'] == 'yes':
+        if host['do you own cats?'] == 'yes':
+            return 0.0
+    
+    # smoking 
+    if guest['are you a smoker?'] == 'yes':
+        if host['are you willing to host smokers?'] == 'no':
+            return 0.0
+    
+    # days available
+    
+    
+    # late night tendencies
+    # preferred guests
+    
+#    return ans
+    return 1.0
 
 def usage():
-    # Example Usage: python housing_assignment_generation.py ./input/guest_data.csv ./input/host_data.csv housing_assignments.json
-    print >> sys.stderr, 'python '+__file__+' guest_data.csv host_data.csv output.json'
+    # Example Usage: python housing_assignment_generation.py ./input/guest_data.tsv ./input/host_data.tsv
+    print >> sys.stderr, 'python '+__file__+' guest_data.tsv host_data.tsv'
     sys.exit(1)
 
 def main():
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 3:
         usage()
-    guest_data_csv_file_name = sys.argv[1]
-    host_data_csv_file_name = sys.argv[2]
-    output_json_file_name = sys.argv[3]
+    os.system('clear')
     
-#    guest_data_json_file_name = remove_file_extension(guest_data_csv_file_name)+'.json'
-#    host_data_json_file_name = remove_file_extension(host_data_csv_file_name)+'.json'
+    guest_data_tsv_file_name = sys.argv[1]
+    host_data_tsv_file_name = sys.argv[2]
     
-    asd
+    guest_keys, guest_list = parse_tsv_file(guest_data_tsv_file_name)
+    host_keys, host_list = parse_tsv_file(host_data_tsv_file_name)
+    
+#    host_guest_pretty_print(guest_list[0])
+#    host_guest_pretty_print(host_list[0])
+    
+    graph = networkx.Graph()
+    
+    for guest in guest_list:
+        for host in host_list:
+            graph.add_edge(host['id'], guest['id'], weight=determine_compatibility(host,guest))
+    
+    maximum_weighted_matching = networkx.max_weight_matching(graph)
+    
+    pdb.set_trace()
 
 if __name__ == '__main__':
     main()
